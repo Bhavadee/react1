2a37c5bd09ea373dac069c70828cdc068e9d122d
bhavadeep reddy <bhavadeep2003@gmail.com>
2022-07-19 19:11:23 -0700

    first react

---
+++ index.js
@@ -0,0 +1,14 @@
+const nav = (
+<nav>
+<div>
+    <h1>bhavi's cafe</h1>
+    <ul>
+        <li>meanu</li>
+        <li>contact</li>
+        <li>about</li>
+    </ul>
+</div>
+</nav>
+)
+
+ReactDOM.render(nav,document.getElementById("root"))
